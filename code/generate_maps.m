241c46c2f18b9e51841cf6b9fc0a0a8c3d1c222a
Elias23 <eliashuwyler@gmx.ch>
2013-10-22 19:10:34 +0200

    New non iterative scaling of the h values 3x faster

--- code/generate_maps.m
+++ code/generate_maps.m
@@ -21,30 +21,37 @@ function [type_map,quality_map,maxquality_map] = generate_maps(Prop)
     %fill arrays
     type_map.array = image(:,:,1);
     maxquality_map.array = image(:,:,3);
-    
-    
+    %scale the values of h to 0:6
+    tic
+    type_map.array = round(mod((type_map.array)*6,6));
+    toc
+%--------------------------------------------------------
+%old iterative version    
+%         %round the hue values to multiples of 1/6
+%         tic
+%          for x = 1:Prop.Sim.world_size/10
+%              for y = 1:Prop.Sim.world_size/10
+%                  if type_map.array(x,y) <= 1/12
+%                      type_map.array(x,y) = 0;
+%                  elseif type_map.array(x,y) <= 3/12
+%                      type_map.array(x,y) = 1/6;
+%                  elseif type_map.array(x,y) <= 5/12
+%                      type_map.array(x,y) = 2/6;
+%                  elseif type_map.array(x,y) <= 7/12
+%                      type_map.array(x,y) = 3/6;
+%                  elseif type_map.array(x,y) <= 9/12
+%                      type_map.array(x,y) = 4/6;
+%                  elseif type_map.array(x,y) <= 11/12
+%                      type_map.array(x,y) = 5/6;
+%                  else
+%                      type_map.array(x,y) = 0;
+%                  end
+%              end
+%          end
+%          toc
+%         type_map.array = round(type_map.array.*6);
+%--------------------------------------------------------
 
-    %round the hue values to multiples of 1/6 
-     for x = 1:Prop.Sim.world_size/10
-         for y = 1:Prop.Sim.world_size/10
-             if type_map.array(x,y) <= 1/12
-                 type_map.array(x,y) = 0;
-             elseif type_map.array(x,y) <= 3/12
-                 type_map.array(x,y) = 1/6;
-             elseif type_map.array(x,y) <= 5/12
-                 type_map.array(x,y) = 2/6;
-             elseif type_map.array(x,y) <= 7/12
-                 type_map.array(x,y) = 3/6;
-             elseif type_map.array(x,y) <= 9/12
-                 type_map.array(x,y) = 4/6;
-             elseif type_map.array(x,y) <= 11/12
-                 type_map.array(x,y) = 5/6;
-             else
-                 type_map.array(x,y) = 0;
-             end
-         end
-     end
-    
     figure
     imagesc(type_map.array);
 end
