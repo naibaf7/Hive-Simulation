18fdde0b997c2a011de32fb4df03f7a8c7c86e5d
Elias <eliashuwyler@gmx.ch>
2013-10-24 18:10:00 +0200

    added comments to code

--- code/draw_map.m
+++ code/draw_map.m
@@ -1,12 +1,21 @@
 function [ output_args ] = draw_map( Prop, type_map, quality_map )
-%DRAW_MAP Summary of this function goes here
-%   Detailed explanation goes here
+%DRAW_MAP draws an image of the map
+%   type_map is the H matrix, and quality_map is the V matrix
+
+%initialize image matrix
 hsv_image_map = ones(Prop.Sim.world_size/10,Prop.Sim.world_size/10,3);
+
+%fill it with the given values
 hsv_image_map(:,:,1) = type_map.array/6;
 hsv_image_map(:,:,3) = quality_map.array;
+
+%convert hsv2rgb
 image_map = hsv2rgb(hsv_image_map);
-image_map = min(image_map,1);
-image_map = max(image_map,0);
+
+%correct 'rounding errors' from hsv2rgb
+image_map = min(max(image_map,0),1);
+
+%display image
 image(image_map)
 
 
